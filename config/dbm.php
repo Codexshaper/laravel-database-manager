669943ee61bf77cda627f7fe9ad2d6406124674d
Md Abu Ahsan Basir <maab.career@gmail.com>
2020-01-08 20:59:10 +0600

    Added Doc comment into config

--- config/dbm.php
+++ config/dbm.php
@@ -1,14 +1,96 @@
 <?php
 return [
+    /*
+    |-------------------------------------------------------------
+    | Prefix
+    |-------------------------------------------------------------
+    |
+    | Set custom URL prefix
+    | by default /database . You can add prefix before /database
+    |
+     */
+
     'prefix'               => '',
+
+    /*
+    |-------------------------------------------------------------
+    | Global namespace
+    |-------------------------------------------------------------
+    |
+    | Set custom global namespace
+    |
+     */
+
     'namespace'            => '\CodexShaper\DBM',
+
+    /*
+    |-------------------------------------------------------------
+    | Controller namespace
+    |-------------------------------------------------------------
+    |
+    | Set custom namespace for controller if you want to override deafults controllers
+    |
+     */
+
     'controller_namespace' => '\CodexShaper\DBM\Http\Controllers',
-    'resources_path'       => 'vendor/codexshaper/laravel-database-manager/publishable/assets/',
-    'views'                => 'vendor/codexshaper/laravel-database-manager/publishable/views',
+
+    /*
+    |-------------------------------------------------------------
+    | Model namespace
+    |-------------------------------------------------------------
+    |
+    | Set custom namespace for models.
+    | It will create new models and load existsing models from this namespace
+    |
+     */
+
     'modal_namespace'      => 'App\\\\',
+
+    /*
+    |-------------------------------------------------------------
+    | Resource path
+    |-------------------------------------------------------------
+    |
+    | Set custom assests path so that you can load your own assets
+    |
+     */
+
+    'resources_path'       => 'vendor/codexshaper/laravel-database-manager/publishable/assets/',
+
+    /*
+    |-------------------------------------------------------------
+    | Views Path
+    |-------------------------------------------------------------
+    |
+    | Set custom assests path so that you can load your own views
+    |
+     */
+
+    'views'                => 'vendor/codexshaper/laravel-database-manager/publishable/views',
+
+    /*
+    |-------------------------------------------------------------
+    | Flesystem
+    |-------------------------------------------------------------
+    |
+    | Set filesystem config
+    |
+     */
+
     'filesystem'           => [
+        'dir'           => 'public/dbm',
         'random_length' => 32,
     ],
+
+    /*
+    |-------------------------------------------------------------
+    | Authentication
+    |-------------------------------------------------------------
+    |
+    | Set auth config
+    |
+     */
+
     'auth'                 => [
         "token" => [
             "expiry" => 24 * 60 * 60 * 1000, // 24 hours as a milliseconds
@@ -19,34 +101,65 @@ return [
             'local_key'    => '_id', // MongoDB
             // 'local_key'    => 'id', // Others
             'display_name' => 'name',
-            'where'        => ['email' => 'admin@admin.com'],
         ],
     ],
+
+    /*
+    |-------------------------------------------------------------
+    | CRUD
+    |-------------------------------------------------------------
+    |
+    | Set crud config
+    |
+     */
+
     'crud'                 => [
+        // Set record
         'record' => [
             'is_modal' => true,
         ],
     ],
+
+    /*
+    |-------------------------------------------------------------
+    | Backup
+    |-------------------------------------------------------------
+    |
+    | Here you can set backup config
+    |
+     */
+
     'backup'               => [
+        // Mysql
         'mysql'                => [
             'binary_path' => "", // c:\\xampp\\mysql\\bin\\
         ],
+        // Sqlite 3
         'sqlite'               => [
             'binary_path' => "", // C:\\sqlite3\\
         ],
+        // Postgree Sql
         'pgsql'                => [
             'binary_path' => "", // C:\\pgsql\\bin\\
         ],
+        // MongoDB
         'mongodb'              => [
             'binary_path' => "", // C:\\Program Files\\MongoDB\\Server\\4.0\bin\\
             // "dsn" => "mongodb+srv://maab:Abuahsan91@laravel-mongodb-t5jhc.mongodb.net/laravel-database-manager",
         ],
+        // Backup Directry in /storage/app/
         'dir'                  => 'backups',
+        // Enable compression. By default true
         'compress'             => true,
+        // Set compressor binary path to execute compression
         'compress_binary_path' => "",
+        // Set compressor extension
         'compress_extension'   => ".gz",
+        // Set compress command
         'compress_command'     => "gzip",
+        // Set uncompress command
         'uncompress_command'   => "gunzip",
-        'debug'                => true,
+        // Enable debug when developemnt mode. By default false
+        'debug'                => false,
     ],
 ];
