4036b593c722c0607d2ddf85ebc0c8f11af542dc
Md Abu Ahsan Basir <maab.career@gmail.com>
2020-01-30 12:27:32 +0600

    Fixed styleci

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -26,7 +26,7 @@ if (! function_exists('dbm_base_path')) {
     {
         return rtrim(config('dbm.base_path'), '/') ?? '';
     }
- }
+}
 
 if (! function_exists('is_json')) {
     function is_json($string)
