b515b2988ae5871a9f36de9a8e2d56435d4dbd9d
Md Abu Ahsan Basir <maab.career@gmail.com>
2020-01-30 12:26:21 +0600

    Fixed styleci

--- config/dbm.php
+++ config/dbm.php
@@ -8,15 +8,12 @@ return [
     |-------------------------------------------------------------
     |
     | Base URL
-    | by default root path. You can add base path /path/to/project/public
-    |
-    | Don't put trailing/end slash(/)
-    |
+    | by default root path. You can add base path /path/to
     |
      */
 
     'base_path'               => '',
-
+    
     /*
     |-------------------------------------------------------------
     | Prefix
@@ -24,10 +21,6 @@ return [
     |
     | Set custom URL prefix
     | by default /database . You can add prefix before /database
-    | Currently supports only empty string. You can use it later
-    |
-    | Don't put trailing/end slash(/)
-    |
     |
      */
 
@@ -76,7 +69,7 @@ return [
     |
      */
 
-    'resources_path'       => 'package/laravel-database-manager/publishable/assets/',
+    'resources_path'       => 'vendor/codexshaper/laravel-database-manager/publishable/assets/',
 
     /*
     |-------------------------------------------------------------
@@ -87,7 +80,7 @@ return [
     |
      */
 
-    'views'                => 'package/laravel-database-manager/publishable/views',
+    'views'                => 'vendor/codexshaper/laravel-database-manager/publishable/views',
 
     /*
     |-------------------------------------------------------------
@@ -113,14 +106,14 @@ return [
      */
 
     'auth'                 => [
-        "token" => [
-            "expiry" => 24 * 60 * 60 * 1000, // 24 hours as a milliseconds
+        'token' => [
+            'expiry' => 24 * 60 * 60 * 1000, // 24 hours as a milliseconds
         ],
         'user'  => [
             'model'        => 'App\\User',
             'table'        => 'users',
-            // 'local_key'    => '_id', // MongoDB
-            'local_key'    => 'id', // Others
+            'local_key'    => '_id', // MongoDB
+            // 'local_key'    => 'id', // Others
             'display_name' => 'name',
         ],
     ],
@@ -153,33 +146,33 @@ return [
     'backup'               => [
         // Mysql
         'mysql'                => [
-            'binary_path' => "", // c:\\xampp\\mysql\\bin\\
+            'binary_path' => '', // c:\\xampp\\mysql\\bin\\
         ],
         // Sqlite 3
         'sqlite'               => [
-            'binary_path' => "", // C:\\sqlite3\\
+            'binary_path' => '', // C:\\sqlite3\\
         ],
         // Postgree Sql
         'pgsql'                => [
-            'binary_path' => "", // C:\\pgsql\\bin\\
+            'binary_path' => '', // C:\\pgsql\\bin\\
         ],
         // MongoDB
         'mongodb'              => [
-            'binary_path' => "", // C:\\Program Files\\MongoDB\\Server\\4.0\bin\\
-            // "dsn" => "mongodb+srv://maab:Abuahsan91@laravel-mongodb-t5jhc.mongodb.net/laravel-database-manager",
+            'binary_path' => '', // C:\\Program Files\\MongoDB\\Server\\4.0\bin\\
+            // 'dsn' => 'mongodb+srv://maab:Abuahsan91@laravel-mongodb-t5jhc.mongodb.net/laravel-database-manager',
         ],
         // Backup Directry in /storage/app/
         'dir'                  => 'backups',
         // Enable compression. By default true
         'compress'             => true,
         // Set compressor binary path to execute compression
-        'compress_binary_path' => "",
+        'compress_binary_path' => '',
         // Set compressor extension
-        'compress_extension'   => ".gz",
+        'compress_extension'   => '.gz',
         // Set compress command
-        'compress_command'     => "gzip",
+        'compress_command'     => 'gzip',
         // Set uncompress command
-        'uncompress_command'   => "gunzip",
+        'uncompress_command'   => 'gunzip',
         // Enable debug when developemnt mode. By default false
         'debug'                => false,
     ],
@@ -188,18 +181,18 @@ return [
     | Core
     |-------------------------------------------------------------
     |
-    | Define core tables that are not allowed to Delete or modify
+    | Here you can set backup config
     |
      */
     'core'                 => [
         'tables' => [
-            "dbm_objects",
-            "dbm_fields",
-            "dbm_permissions",
-            "dbm_user_permissions",
-            "dbm_collection_fields",
-            "dbm_collections",
-            "dbm_templates",
+            'dbm_objects',
+            'dbm_fields',
+            'dbm_permissions',
+            'dbm_user_permissions',
+            'dbm_collection_fields',
+            'dbm_collections',
+            'dbm_templates',
         ],
     ],
 
